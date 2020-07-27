01f462bccce031f9824cb7602be49ea5b6396383
samuelson <samuelson@samuelson-ThinkPad-Edge-E545>
2020-07-27 02:40:08 -0600

    Graficos ya incluidos update

--- src/main/java/ComparacionDeCadenas/Main.java
+++ src/main/java/ComparacionDeCadenas/Main.java
@@ -14,15 +14,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Este es el main... sin logica ni nada");
-        System.out.println("Aun esta vacio");
-        CompararCadenas compararCadenas = new CompararCadenas();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingrese primera cadena");
-        String primera = scanner.nextLine();
-        System.out.println("Ingrese segunda cadena");
-        String segunda = scanner.nextLine();
-
-        System.out.println("Cadena mas larga: " + compararCadenas.compararCadenas(primera, segunda));
+        Graficos graficos = new Graficos();
     }
 }
