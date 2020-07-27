62b461a7bddbd8cd0edb0c36ba5d12b3cb9c7df1
samuelson <samuelson@samuelson-ThinkPad-Edge-E545>
2020-07-27 00:54:30 -0600

    Primer commit, solo Main

---
+++ src/main/java/ComparacionDeCadenas/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ComparacionDeCadenas;
+
+/**
+ *
+ * @author samuelson
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Este es el main... sin logica ni nada");
+        System.out.println("Aun esta vacio");
+    }
+}
